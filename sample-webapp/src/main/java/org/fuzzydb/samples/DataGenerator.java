d3b37acd8fa74721ae2e57a9d6ad94169a6a8936
Neale Upstone <neale@nealeupstone.com>
2011-09-07 08:27:34 +0100

    Add randomisation of smoke and newspapers attributes

--- sample-webapp/src/main/java/org/fuzzydb/samples/DataGenerator.java
+++ sample-webapp/src/main/java/org/fuzzydb/samples/DataGenerator.java
@@ -25,8 +25,10 @@ public class DataGenerator implements InitializingBean {
 	
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		randomSource.configureFloatAttr("age", 13f, 100f, 0);
-		randomSource.configureFloatAttr("salary", 5000f, 1e6f, 10);
+		randomSource.configureFloatAttr("age", 13f, 100f, 0f);
+		randomSource.configureFloatAttr("salary", 5000f, 1e6f, 0.05f);
+		randomSource.configureEnumAttr("smoke", 0.05f);
+		randomSource.configureMultiEnumAttr("newspapers", 0.01f);
 	}
 
 	
@@ -40,6 +42,8 @@ public class DataGenerator implements InitializingBean {
 		addRandomAttr(item, "isMale");
 		addRandomAttr(item, "age");
 		addRandomAttr(item, "salary");
+		addRandomAttr(item, "newspapers");
+		addRandomAttr(item, "smoke");
 		
 		return item;
 	}
@@ -54,39 +58,6 @@ public class DataGenerator implements InitializingBean {
 		matt.setAttr("smoke", "Cigar-smoker");
 		matt.setAttr("newspapers", new String[]{"LA Times", "New York Times"});
 		people.put("Matt", matt);
-		
-		FuzzyItem angelina = new FuzzyItem("Angelina");
-		angelina.setAttr("isMale", Boolean.FALSE);
-		angelina.setAttr("age", 35f);
-		angelina.setAttr("ageRange", new float[]{30f, 37f, 50f});
-		angelina.setAttr("salary", 500000f);
-		angelina.setAttr("smoke", "Cigarette-smoker");
-		angelina.setAttr("newspapers", new String[]{"Guardian", "New York Times"});
-		people.put("Angelina", angelina);
-
-		FuzzyItem brad = new FuzzyItem("Brad");
-		brad.setAttr("isMale", Boolean.TRUE);
-		brad.setAttr("age", 37f);
-		brad.setAttr("ageRange", new float[]{22f, 30f, 40f});
-		brad.setAttr("salary", 547000f);
-		brad.setAttr("newspapers", new String[]{"LA Times"});
-		people.put("Brad", brad);
-
-		FuzzyItem neale = new FuzzyItem("Neale");
-		neale.setAttr("isMale", Boolean.TRUE);
-		neale.setAttr("age", 21f); // I wish (sort of)
-		neale.setAttr("salary", 25000f);
-		neale.setAttr("smoke", "Non-smoker");
-		neale.setAttr("newspapers", new String[]{"Guardian"});
-		people.put("Neale", neale);
-
-		FuzzyItem wayne = new FuzzyItem("Wayne");
-		wayne.setAttr("isMale", Boolean.TRUE);
-		wayne.setAttr("age", 33f); 
-		wayne.setAttr("salary", 2500000f);
-		wayne.setAttr("smoke", "Non-smoker");
-		wayne.setAttr("newspapers", new String[]{"Sun"});
-		people.put("Wayne", wayne);
 	}
 
 	private void addRandomAttr(FuzzyItem item, String attr) {
