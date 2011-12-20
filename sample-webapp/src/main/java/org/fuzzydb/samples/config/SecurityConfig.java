8061f3fb4469bd74322d94296233414a8606c841
Neale Upstone <neale.upstone@opencredo.com>
2011-12-20 19:38:13 +0000

    Update security context based on example in Spring Social

---
+++ sample-webapp/src/main/java/org/fuzzydb/samples/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.fuzzydb.samples.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Security Configuration.
+ */
+@Configuration
+@ImportResource("classpath:/security-context.xml")
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+    	return NoOpPasswordEncoder.getInstance();
+	}
+    
+	@Bean
+	public TextEncryptor textEncryptor() {
+		return Encryptors.noOpText();
+	}
+
+}
