c9e60439e8216e4ccc694b2aece127f49d6a12e1
Neale Upstone <neale.upstone@opencredo.com>
2011-12-21 10:04:27 +0000

    Add @JavaConfig config for Social and Sql beans

---
+++ sample-webapp/src/main/java/org/fuzzydb/samples/config/MainConfig.java
@@ -0,0 +1,19 @@
+package org.fuzzydb.samples.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Turns on @Component scanning, loads application.properties, and enables Spring transaction management
+ */
+@Configuration
+@ComponentScan(basePackages = "org.fuzzydb.samples.social", excludeFilters = { @Filter(Configuration.class) })
+@PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
+//@ImportResource("classpath:/fuzzy-repository-context.xml")
+public class MainConfig {
+
+}
---
+++ sample-webapp/src/main/java/org/fuzzydb/samples/config/SqlDatabaseConfig.java
@@ -0,0 +1,39 @@
+package org.fuzzydb.samples.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Config SQL data sources
+ */
+@Configuration
+@EnableTransactionManagement
+public class SqlDatabaseConfig {
+
+	@Bean(destroyMethod = "shutdown")
+	public DataSource dataSource() {
+		EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
+		factory.setDatabaseName("fuzzydb-sample-webapp");
+		factory.setDatabaseType(EmbeddedDatabaseType.H2);
+		factory.setDatabasePopulator(databasePopulator());
+		return factory.getDatabase();
+	}
+
+	// internal helpers
+
+	private DatabasePopulator databasePopulator() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("JdbcUsersConnectionRepository.sql", JdbcUsersConnectionRepository.class));
+		return populator;
+	}
+
+}
