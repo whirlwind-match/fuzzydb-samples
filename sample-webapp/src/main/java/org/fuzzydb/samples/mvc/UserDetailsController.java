74cbfa0bb0aafcce15e045fb3ea464c38c00f137
Neale Upstone <neale.upstone@opencredo.com>
2011-12-24 09:45:28 +0000

    Clean up text

--- sample-webapp/src/main/java/org/fuzzydb/samples/mvc/UserDetailsController.java
+++ sample-webapp/src/main/java/org/fuzzydb/samples/mvc/UserDetailsController.java
@@ -33,7 +33,7 @@ public class UserDetailsController {
 	public SignupForm signupForm(WebRequest request) {
 		Connection<?> connection = ProviderSignInUtils.getConnection(request);
 		if (connection != null) {
-			request.setAttribute("message", new Message(MessageType.INFO, "Your " + StringUtils.capitalize(connection.getKey().getProviderId()) + " account is not associated with a Spring Social Showcase account. If you're new, please sign up."), WebRequest.SCOPE_REQUEST);
+			request.setAttribute("message", new Message(MessageType.INFO, "Your " + StringUtils.capitalize(connection.getKey().getProviderId()) + " identity is not associated with an account. If you're new, please sign up."), WebRequest.SCOPE_REQUEST);
 			return SignupForm.fromProviderUser(connection.fetchUserProfile());
 		} else {
 			return new SignupForm();
