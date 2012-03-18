752758b184584525fd275be0f4171a90fcf481e1
Neale Upstone <neale@nealeupstone.com>
2012-03-18 19:29:59 +0000

    Move servlet context to classpath and remove root-context (which just imports another)

--- sample-webapp/src/test/java/org/fuzzydb/samples/mvc/SearchControllerIntegrationTest.java
+++ sample-webapp/src/test/java/org/fuzzydb/samples/mvc/SearchControllerIntegrationTest.java
@@ -12,12 +12,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * Test that our Spring context is set up correctly
+ * Test that our Spring context is set up correctly.  This is not as easy as it should be. I want:
+ * <pre>
+ * {@code @ContextConfiguration}( 
+ *    locations = {"root-context.xml"}, 
+ *    child = {@code @ContextConfiguration}(locations = "/WEB-INF/servlet.xml") 
+ *    )
+ * </pre>
+ * This would produce a child servlet context with a parent.  It's almost impossible to correctly test
+ * servlet controller configurations without this.<br>
+ * This approach would also allow a more fine grained {@code @DirtiesContext} behaviour if also providing a
+ * name attribute on each context (i.e. name="root", name="servlet")
+ * 
+ *  In XML, <parent classes="..." or resources="..." /> would create a parent classloader.  Unfortunately, parents are expected to already
+ *  exist. siblings would be possible... no.. wait.  I could create an application context, initialise it, and then register every concrete bean...
+ *  
  * @author Neale
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:fuzzy-repository-context.xml","classpath:controllers.xml"})
+@ContextConfiguration(locations={"classpath:test-context.xml"})
 public class SearchControllerIntegrationTest {
 	
 	@Autowired
